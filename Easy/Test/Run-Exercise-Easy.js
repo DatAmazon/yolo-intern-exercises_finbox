97fb05325659b09ee57c73692c433b899ab52b13
DatGit <kieuducdat2k@gmail.com>
2021-12-01 08:57:07 +0700

    File run Test

---
+++ Easy/Test/Run-Exercise-Easy.js
@@ -0,0 +1,38 @@
+const checkExercise1 = require('./Test-Exercise-1');
+const checkExercise2 = require('./Test-Exercise-2');
+const checkExercise3 = require('./Test-Exercise-3');
+const checkExercise4 = require('./Test-Exercise-4');
+const checkExercise5 = require('./Test-Exercise-5');
+const checkExercise6 = require('./Test-Exercise-6');
+const checkExercise7 = require('./Test-Exercise-7');
+const checkExercise8 = require('./Test-Exercise-8');
+const checkExercise9 = require('./Test-Exercise-9');
+const checkExercise10 = require('./Test-Exercise-10');
+const checkExercise11 = require('./Test-Exercise-11');
+const checkExercise12 = require('./Test-Exercise-12');
+const checkExercise13 = require('./Test-Exercise-13');
+const checkExercise14 = require('./Test-Exercise-14');
+const checkExercise15 = require('./Test-Exercise-15');
+
+function checkAll() {
+  console.log('Start easy');
+  checkExercise1();
+  checkExercise2();
+  checkExercise3();
+  checkExercise4();
+  checkExercise5();
+  checkExercise6();
+  checkExercise7();
+  checkExercise8();
+  checkExercise9();
+  checkExercise10();
+  checkExercise11();
+  checkExercise12();
+  checkExercise13();
+  checkExercise14();
+  checkExercise15();
+  console.log('Done easy');
+}
+checkAll();
+
+
